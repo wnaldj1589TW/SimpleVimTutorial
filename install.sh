dec57519707c1c65acdf7cc92271ae229733ae98
wnaldj1589TW <wnaldj1589@twinny.co.kr>
2020-04-10 09:15:20 +0900

    Fix relative path to absolute path - #2

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RELATIVE_DIR=`dirname "$0"`
+CUR_PATH=`readlink -f $RELATIVE_DIR`
 
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4AB0F789CBA31744CC7DA76A8CF63AD3F06FC659
@@ -8,7 +10,7 @@ sudo apt install -y vim clang-format
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cat vimrc >> ~/.vimrc
-cat plugin_list >> ~/.vimrc
+cat $CUR_PATH/vimrc >> ~/.vimrc
+cat $CUR_PATH/plugin_list >> ~/.vimrc
 
 vim +slient +VimEnter +PlugInstall +qall
